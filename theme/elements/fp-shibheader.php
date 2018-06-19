cf6f0f0e8e451c5d1830503c0cf747d943464fe5
Kazuki Fukuda <nisshin@dtdsh.com>
2018-06-19 17:30:07 +0900

    事務所総数と挨拶文変更

--- theme/elements/fp-shibheader.php
+++ theme/elements/fp-shibheader.php
@@ -7,11 +7,11 @@
 	);
 	$slider = new WP_Query( $slides_args );
 ?>
-<section class="fp--topheader">
-	<div class="row align-middle">
-		<div class="column small-12 medium-6 slider--wrapper">
-			<div id="js--top-slider" class="slick-slider">
-			<?php
+	<section class="fp--topheader">
+		<div class="row align-middle">
+			<div class="column small-12 medium-6 slider--wrapper">
+				<div id="js--top-slider" class="slick-slider">
+					<?php
 				if ( $slider->have_posts() ) :
 					while ( $slider->have_posts() ) :
 						$slider->the_post();
@@ -23,36 +23,58 @@
 							$link_src = get_post_meta( $id, 'slider_link', true );
 							$link_title = ( get_post_meta( $id, 'slider_link_title', true ) ) ? get_post_meta( $id, 'slider_link_title', true ) : get_the_title();
 				?>
-				<article class="slick-slide slide--item card">
-					<div class="card--img"><img src="<?php echo $img; ?>" alt="<?php the_title(); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>"></div>
-					<div class="card--content">
-						<p class="card--title"><?php the_title(); ?></p>
-						<p><?php
+						<article class="slick-slide slide--item card">
+							<div class="card--img">
+								<img src="<?php echo $img; ?>" alt="<?php the_title(); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
+							</div>
+							<div class="card--content">
+								<p class="card--title">
+									<?php the_title(); ?>
+								</p>
+								<p>
+									<?php
 							if ( '' !== $post->post_content ) {
 								the_content();
 							} else {
 								echo "<a href=\"$link_src\" title=\"$link_title\" class=\"button\">$link_title</a>";
 							}
-						?></p>
-					</div>
-				</article>
-				<?php
+						?>
+								</p>
+							</div>
+						</article>
+						<?php
 						endwhile;
 					endif;
 				?>
-				<div id="slide--button"></div>
+						<div id="slide--button"></div>
+				</div>
+			</div>
+			<div class="column small-12 medium-6 copy--top text-center">
+				<h1 class="copy--title">山下江法律事務所
+					<br>呉支部</h1>
+				<div class="copy--desc">
+					<p>相談件数17,000件以上。
+						<small>※</small>
+						<br>選ぶなら広島最大級。</p>
+					<small>※　平成29年12月時点（事務所総数）</small>
+				</div>
+				<p class="topheader--button">
+					<a href="tel:0120783409" title="電話する" class="button hollow expanded">
+						<img src="//www.law-yamashita.com/wp-content/themes/ym-home/assets/img/header-tel.jpg" alt="">
+					</a>
+				</p>
+				<p class="topheader--button">
+					<a href="https://www.law-yamashita.com/contact" class="button hollow expanded" title="メールで相談予約" target="_blank">
+						<?php NID_SVG::icon( 'mail', array( 'class' => 'button--icon' ), 'メール' ); ?>メールで相談予約</a>
+				</p>
+				<p class="topheader--button button-group expanded stacked-for-small">
+					<a href="<?php echo get_page_link( '6226' ); ?>" class="button hollow expanded" title="取扱範囲">取扱範囲</a>
+					<a href="#front--news" class="button hollow expanded" title="お知らせ">お知らせ</a>
+				</p>
+				<p class="topheader--button">
+					<a href="https://www.law-yamashita.com" target="_blank" class="button hollow expanded" title="メインサイトを見る">
+						<?php NID_SVG::icon( 'home', array( 'class' => 'button--icon' ), 'メインサイトを見る' ); ?>メインサイトを見る</a>
+				</p>
 			</div>
 		</div>
-		<div class="column small-12 medium-6 copy--top text-center">
-			<h1 class="copy--title">山下江法律事務所<br>呉支部</h1>
-			<div class="copy--desc">
-				<p>相談件数17,000件以上。<small>※</small><br>選ぶなら広島最大級。</p>
-			<small>※　平成29年12月時点</small>
-			</div>
-			<p class="topheader--button"><a href="tel:0120783409" title="電話する" class="button hollow expanded"><img src="//www.law-yamashita.com/wp-content/themes/ym-home/assets/img/header-tel.jpg" alt=""></a></p>
-			<p class="topheader--button"><a href="https://www.law-yamashita.com/contact" class="button hollow expanded" title="メールで相談予約" target="_blank"><?php NID_SVG::icon( 'mail', array( 'class' => 'button--icon' ), 'メール' ); ?>メールで相談予約</a></p>
-			<p class="topheader--button button-group expanded stacked-for-small"><a href="<?php echo get_page_link( '6226' ); ?>" class="button hollow expanded" title="取扱範囲">取扱範囲</a><a href="#front--news" class="button hollow expanded" title="お知らせ">お知らせ</a></p>
-			<p class="topheader--button"><a href="https://www.law-yamashita.com" target="_blank" class="button hollow expanded" title="メインサイトを見る"><?php NID_SVG::icon( 'home', array( 'class' => 'button--icon' ), 'メインサイトを見る' ); ?>メインサイトを見る</a></p>
-		</div>
-	</div>
-</section>
+	</section>
--- theme/elements/fp-welcome-mess.php
+++ theme/elements/fp-welcome-mess.php
@@ -29,17 +29,20 @@
 		<div class="row align-middle">
 			<div class="column small-12 medium-6 large-4 message--image">
 				<img class="thumbnail" src="https://www.law-yamashita.com/wp-content/uploads/2015/12/miyabe.jpg" width="210" height="210"
-				    alt=呉支部長・弁護士の宮部明典 ">
+				    alt="呉支部長・弁護士の宮部明典">
 			</div>
-			<div class="column small-12 medium-6 large-8 ">
-				<h2 class="title--border-bottom ">呉支部長のご挨拶</h2>
-				<div class="message--paragraph ">
+			<div class="column small-12 medium-6 large-8">
+				<h2 class="title--border-bottom">呉支部長のご挨拶</h2>
+				<div class="message--paragraph">
 					<p>平成29年1月に呉支部を開所以来、多くの方にご相談いただいております。</p>
 					<p>当事務所に相談したかったけれども広島まで出かけられないという呉地域居住の方も多くいらっしゃいました。そのような方々からは、悩みを相談できる事務所が住んでいるところの近くにできてありがたいというお言葉をたくさんかけていただきました。今後も呉地域居住の方が抱える悩みに寄り添い、適切な解決を導いていきたいと思います。</p>
-					<p>若輩者ではございますが、みなさまのご期待に応えられるよう日々努力していきますので、今後ともご指導ご鞭撻のほどよろしくお願いします。</p>
-					<p class="button-group "><a class="js--modal-button button " data-ajax-modal="https://www.law-yamashita.com/wp-json/wp/v2/members/2906
-				    " data-toggle="js--ajax-modal ">宮部明典について　></a><a href="https://www.facebook.com/lawyermiyabeakinori/ " class="button bg-facebook
-				    " target="_blank ">呉支部のFacebook</a></p>
+					<p>みなさまのご期待に応えられるよう日々努力していきますので、今後ともご指導ご鞭撻のほどよろしくお願いします。</p>
+					<p class="button-group">
+						<a class="js--modal-button button" data-ajax-modal="https://www.law-yamashita.com/wp-json/wp/v2/members/2906
+				" data-toggle="js--ajax-modal">宮部明典について　></a>
+						<a href="https://www.facebook.com/lawyermiyabeakinori/" class="button bg-facebook
+				" target="_blank">呉支部のFacebook</a>
+					</p>
 				</div>
 			</div>
 		</div>
